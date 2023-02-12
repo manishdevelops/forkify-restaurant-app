9cda95a311ad296764e7984bcc1992bad26b36d3
manish mandal <76776194mm@gmail.com>
2023-02-12 12:48:54 +0530

    update all

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,6 +28,7 @@ const controlRecipes = async function() {
     recipeView.render(model.state.recipe);
   } catch(err) {
     console.log(err);
+    recipeView.renderError();
   }
 }
 
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,6 +5,8 @@ import {Fraction} from 'fractional';
  class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = `We couldn't find that recipe. Please try another one!`;
+  #message = '';
 
   render(data) {
     this.#data = data;
@@ -17,7 +19,7 @@ import {Fraction} from 'fractional';
     this.#parentElement.innerHTML = '';
   }
 
-  renderSpinner = function() {
+  renderSpinner() {
     const markup = `
           <div class="spinner">
             <svg>
@@ -25,9 +27,40 @@ import {Fraction} from 'fractional';
             </svg>
           </div>
     `;
-    this.#parentElement.innerHTML = '';
+    this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+         <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+         <div class="message">
+            <div>
+              <svg>
+                <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
 
   addHandlerRender(handler) {
     // window.addEventListener('hashchange', handler);
